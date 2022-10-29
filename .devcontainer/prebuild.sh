eefe2a71fa669bf2c22abcdc3e7933185e74c178
Tomoki Kawai <ooiaai424@gmail.com>
2022-10-29 15:09:49 +0900

    fix group name

--- .devcontainer/prebuild.sh
+++ .devcontainer/prebuild.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # get parameter from system
 user=developer
-group=`id -u`
+group=`id -gn`
 uid=`id -u`
 gid=`id -g`
-docker image build -t foxy_pre:latest -f Dockerfile.pre\
---build-arg USER=${user} \
---build-arg UID=${uid} \
---build-arg GROUP=${group} \
---build-arg GID=${gid} .
-
-
-
+docker image build -t foxy_pre:latest -f Dockerfile.pre \
+	--build-arg USER=${user} \
+	--build-arg UID=${uid} \
+	--build-arg GROUP=${group} \
+	--build-arg GID=${gid} .
